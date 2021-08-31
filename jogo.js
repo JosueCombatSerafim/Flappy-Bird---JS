406fe65cb5eb8b4bc2ab2378f0cbb2048f9eecd5
JosueCombatSerafim <wrj.serafim@bol.com.br>
2021-08-31 19:54:44 -0300

    feat: add all background and items and add gravity

--- jogo.js
+++ jogo.js
@@ -5,3 +5,123 @@ sprites.src = "./sprites.png";
 
 const canvas = document.querySelector("canvas");
 const contexto = canvas.getContext("2d");
+
+// [Plano de Fundo]
+const planoDeFundo = {
+  spriteX: 390,
+  spriteY: 0,
+  largura: 275,
+  altura: 204,
+  x: 0,
+  y: canvas.height - 204,
+  desenha() {
+    contexto.fillStyle = "#70c5ce";
+    contexto.fillRect(0, 0, canvas.width, canvas.height);
+
+    contexto.drawImage(
+      sprites,
+      planoDeFundo.spriteX,
+      planoDeFundo.spriteY,
+      planoDeFundo.largura,
+      planoDeFundo.altura,
+      planoDeFundo.x,
+      planoDeFundo.y,
+      planoDeFundo.largura,
+      planoDeFundo.altura
+    );
+
+    contexto.drawImage(
+      sprites,
+      planoDeFundo.spriteX,
+      planoDeFundo.spriteY,
+      planoDeFundo.largura,
+      planoDeFundo.altura,
+      planoDeFundo.x + planoDeFundo.largura,
+      planoDeFundo.y,
+      planoDeFundo.largura,
+      planoDeFundo.altura
+    );
+  },
+};
+
+// [Chao]
+const chao = {
+  spriteX: 0,
+  spriteY: 610,
+  largura: 224,
+  altura: 112,
+  x: 0,
+  y: canvas.height - 112,
+  desenha() {
+    contexto.drawImage(
+      sprites,
+      chao.spriteX,
+      chao.spriteY,
+      chao.largura,
+      chao.altura,
+      chao.x,
+      chao.y,
+      chao.largura,
+      chao.altura
+    );
+
+    contexto.drawImage(
+      sprites,
+      chao.spriteX,
+      chao.spriteY,
+      chao.largura,
+      chao.altura,
+      chao.x + chao.largura,
+      chao.y,
+      chao.largura,
+      chao.altura
+    );
+  },
+};
+
+// [FlappyBird]
+const flappyBird = {
+  spriteX: 0,
+  spriteY: 0,
+  width: 33,
+  height: 24,
+  x: 10,
+  y: 50,
+
+  velocidade: 0,
+  gravidade: 0.25,
+  atualiza() {
+    flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade;
+    flappyBird.y = flappyBird.y + this.velocidade;
+  },
+
+  desenha() {
+    contexto.drawImage(
+      sprites, // image
+
+      flappyBird.spriteX, // sprite X
+      flappyBird.spriteY, // sprite Y
+
+      flappyBird.width, // sprite Width
+      flappyBird.height, // sprite Height,
+
+      flappyBird.x, // dentro do canvas X
+      flappyBird.y, // dentro do canvas Y
+
+      flappyBird.width, // dentro do canvas Width
+      flappyBird.height // dentro do canvas Height
+    );
+  },
+};
+
+// Game
+function loop() {
+  planoDeFundo.desenha();
+  chao.desenha();
+  flappyBird.atualiza();
+  flappyBird.desenha();
+
+  requestAnimationFrame(loop);
+}
+
+loop();
